b6311c43c157c55719796517b6858e0d64d4dbba
abrjak <jakob.abraham@gibmit.ch>
2018-02-05 13:35:10 +0100

    Added README and minor Chnages in Accessibility-Subject

---
+++ src/ch/baloise/it/theory/accessibility/ClassVariablesTest.java
@@ -0,0 +1,23 @@
+package ch.baloise.it.theory.accessibility;
+
+/**
+ * 
+ * @author Jakob Abraham
+ *
+ */
+public class ClassVariablesTest {
+	
+	public static void main(String[] args) {
+		new ClassVariablesTest();
+	}
+	
+	public ClassVariablesTest() {
+		
+		ClassVariables cv = new ClassVariables();
+		System.out.println(cv.firstname);
+		System.out.println(cv.lastname);
+		
+		
+	}
+
+}
